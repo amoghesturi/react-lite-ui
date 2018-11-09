821301235be7ef3461006effdfe91c72486394bb
rishichawda <chawda.rishii@gmail.com>
2018-11-09 11:26:07 +0530

    🎨  (src/modal)-remove vanilla code and add reactive code

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 import { themr } from 'react-css-themr';
 import cx from 'classnames';
 import defaultTheme from './theme.scss';
 import '../globals/fonts.scss';
 
+const getDOMNode = findDOMNode;
+
 class Modal extends Component {
   constructor(props) {
     super(props);
+    this.modalWrapperRef = null;
+    this.modalRef = null;
     this.state = {
       open: props.open,
     };
@@ -20,8 +25,8 @@ class Modal extends Component {
   }
 
   closeModal = () => {
-    const modalWrapper = document.querySelector('#modalWrapper');
-    const modal = document.querySelector('#modal');
+    const modalWrapper = getDOMNode(this.modalWrapperRef);
+    const modal = getDOMNode(this.modalRef);
     modalWrapper.classList.add('animation');
     modal.classList.add('animation');
     setTimeout(() => {
@@ -43,7 +48,7 @@ class Modal extends Component {
       );
     }
     return null;
-  }
+  };
 
   renderModalFooter = (footer) => {
     const { theme } = this.props;
@@ -55,7 +60,7 @@ class Modal extends Component {
       );
     }
     return null;
-  }
+  };
 
   render() {
     const {
@@ -83,16 +88,28 @@ class Modal extends Component {
       <div
         className={backdrop}
         onClick={closeOnBackdropClick ? this.closeModal : undefined}
-        id="modalWrapper"
+        ref={(ref) => {
+          this.modalWrapperRef = ref;
+        }}
       >
         {open && (
-          <div id="modal" className={classes} onClick={e => e.stopPropagation()}>
+          <div
+            id="modal"
+            className={classes}
+            onClick={e => e.stopPropagation()}
+            ref={(ref) => {
+              this.modalRef = ref;
+            }}
+          >
             {this.renderModalTitle(title)}
             <div className={theme['modal-body']} aria-label="card-body">
               {children || body || null}
             </div>
             {this.renderModalFooter(footer)}
-            <i className={cx(theme.close, 'icon-cross')} onClick={this.closeModal} />
+            <i
+              className={cx(theme.close, 'icon-cross')}
+              onClick={this.closeModal}
+            />
           </div>
         )}
       </div>
